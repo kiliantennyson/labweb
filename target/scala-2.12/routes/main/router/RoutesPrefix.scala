83e0aa132d61d087336788a2fa28b14c6e4d7d3e
kiliantennyson <x00104769@gmail.com>
2017-11-16 12:00:45 +0000

    first commit

---
+++ target/scala-2.12/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,16 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/home/wdd/webapps/playapps/helloworld/conf/routes
+// @DATE:Thu Nov 16 11:51:50 GMT 2017
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
